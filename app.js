cd8c8c408ab2e3111307f0d28a9c62277d54507a
Felix lie <felixlie19@gmail.com>
2021-12-26 10:18:11 -0800

    styling with bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
@@ -25,6 +26,7 @@ db.once('open', () => {
 
 const app = express()
 
+app.engine('ejs', ejsMate) //using this to styling boilerplates
 app.set('view engine', 'ejs') //to enable ejs files
 app.set('views', path.join(__dirname, 'views')) //to help with directories
 
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelper')
 const Campground = require('../models/campground')
+const { toLocaleString } = require('./cities')
 
 mongoose
   .connect('mongodb://localhost:27017/yelp-camp', {
@@ -29,9 +30,14 @@ const seedDB = async () => {
   const c = new Campground({ title: 'purple field' })
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis et voluptatem labore, sapiente molestias id porro est corrupti. Quod sit corrupti consectetur est hic? Culpa illum qui aliquid excepturi ullam!',
+      price: price,
     })
     await camp.save()
   }
