064f77ecec2e702a960950c00df4f6d5b8d8d089
Felix lie <felixlie19@gmail.com>
2022-03-29 16:00:15 -0700

    Add GeoJSON

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -26,16 +26,17 @@ module.exports.createCampground = async (req, res, next) => {
       limit: 1,
     })
     .send();
-  // const campground = new Campground(req.body.campground);
-  // campground.images = req.files.map((f) => ({
-  //   url: f.path,
-  //   filename: f.filename,
-  // }));
-  // campground.author = req.user._id;
-  // await campground.save();
-  // console.log(campground);
-  // req.flash('success', 'Successfully Made a new Campground');
-  // res.redirect(`/campgrounds/${campground._id}`);
+  const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
+  campground.author = req.user._id;
+  await campground.save();
+  console.log(campground);
+  req.flash('success', 'Successfully Made a new Campground');
+  res.redirect(`/campgrounds/${campground._id}`);
 };
 
 //Controller to render to a specific camp
