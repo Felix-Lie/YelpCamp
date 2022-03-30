97d1b9947fc3de738ee3336ce911777553c71fef
Felix lie <felixlie19@gmail.com>
2022-03-30 00:04:30 -0700

    Centering the Map on a Campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,11 @@
+// const campground = require('../../models/campground');
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 4, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
