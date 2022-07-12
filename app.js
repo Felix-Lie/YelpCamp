f98337ebfe5125e4a5f452c09fb1380b0dce9dd5
Felix lie <felixlie19@gmail.com>
2022-07-11 20:26:53 -0700

    Minor changes to Session/Cookies

--- app.js
+++ app.js
@@ -56,11 +56,13 @@ app.use(
 
 //session
 const sessionConfig = {
+  name: 'session',
   secret: 'thisshouldbeabettersecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
